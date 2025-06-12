02f4dd319d7813b1cbf56fc6e015999d7110fc0d
Vibhor Bhatt <108974586+vibhorbhatt777@users.noreply.github.com>
2025-06-12 15:52:00 +0530

    0015-3SUM Stats: Time: 619 ms (87%), Space: 18.4 MB (40.71%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,30 +5,46 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
+        result = []
         n = len(nums)
-        res= []
-        for i in range(n):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
-            else:
-                j=i+1
-                k=n-1
-                while(j<k):
-                    sums = nums[i]+nums[j]+nums[k]
-                    if sums>0:
-                        k-=1
-                    elif sums<0:
-                        j+=1
-                    else:
-                        res.append([nums[i],nums[j],nums[k]])
-                        j+=1
-                        k-=1
-                        while(j<k and nums[j]==nums[j-1]):
-                            j+=1
-                        while(j<k and nums[k]==nums[k+1]):
-                            k-=1
 
-        return res
+        for i in range(n-2): 
+            # fix first element (need atleast 3 element)
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            #now use twin walker approach - inward traversal
+            left , right = i+1 , n-1
+            target = -nums[i] # i want nums[left] + nums[right] = -nums[i]
+
+            while left < right:
+                curr_sum = nums[left] + nums[right]
+
+                if curr_sum == target:
+                    result.append([nums[i],nums[left],nums[right]])
+
+                    #skip duplicates for left pointer
+                    while left < right and nums[left] == nums[left+1]:
+                        left +=1
+
+                    #skip duplicates for right pointer
+                    while left < right and nums[right] == nums[right-1]:
+                        right-=1
+                    
+                    left+=1
+                    right -=1
+                
+                elif curr_sum < target:
+                    left+=1
+                else:
+                    right-=1
+        
+        return result
+
+
+
+
+        
 
         
         
