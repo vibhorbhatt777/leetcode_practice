32e05cd88f1169b875bee4112d6a0724349432b1
Vibhor Bhatt <108974586+vibhorbhatt777@users.noreply.github.com>
2025-06-30 19:33:33 +0530

    1538-MAXIMUM-POINTS-YOU-CAN-OBTAIN-FROM-CARDS Stats: Time: 25 ms (58.63%), Space: 19.3 MB (77.64%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -1,42 +1,20 @@
 class Solution(object):
-    def maxScore(self, cardPoints, k):
-        """
-        :type cardPoints: List[int]
-        :type k: int
-        :rtype: int
-        """
-        n = len(cardPoints)
-        max_score = 0
-        left = 0
-        right = 0
-        window_size = n - k
-        min_window_sum = float('inf')
+    def maxScore(self, nums, k):
+        n = len(nums)
         if n == k:
-            return sum(cardPoints)
+            return sum(nums)
+                 
+        # Start with all k cards from left
+        left_sum = sum(nums[:k])
+        right_sum = 0
+        maxi = left_sum
         
-        total_sum = sum(cardPoints)
-        
-        while right < n:
-            max_score += cardPoints[right]
-        
-            while right - left + 1 > window_size:
-
-                max_score -= cardPoints[left]
-                left += 1
-        
-            if right - left + 1 == window_size:
-
-                min_window_sum = min(min_window_sum, max_score)
-            
-            right += 1
-        
-        return total_sum - min_window_sum
-        
-
-
-
-
-
-
-
-
+        # Try combinations: remove from left, add from right
+        right_index = n - 1
+        for i in range(k-1, -1, -1):  # Remove from position k-1 down to 0
+            left_sum -= nums[i]      # Remove card from left
+            right_sum += nums[right_index]  # Add card from right
+            maxi = max(maxi, left_sum + right_sum)
+            right_index -= 1
+                  
+        return maxi
